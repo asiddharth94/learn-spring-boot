87812c452a1dc9eefe557d04b92e0132f8b649d3
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2023-10-03 00:36:25 +0100

    spring boot starter demo

---
+++ learnspring/src/main/java/com/springboot/demo/learnspring/LearnspringApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.learnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringApplication.class, args);
+	}
+
+}
