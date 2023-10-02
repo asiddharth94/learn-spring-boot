bc451a89160d8102ef9198854a5b66b6e8e46dac
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2023-10-03 00:53:11 +0100

    annotations - value and reading from properties file

--- learnspring/src/main/java/com/springboot/demo/learnspring/rest/DemoController.java
+++ learnspring/src/main/java/com/springboot/demo/learnspring/rest/DemoController.java
@@ -1,13 +1,16 @@
 package com.springboot.demo.learnspring.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${project.default-mapping}")
+    private String defalutMapping;
     @GetMapping("/")
     public String renderDefaultRoute() {
-        return "Learning Spring";
+        return defalutMapping;
     }
 }
