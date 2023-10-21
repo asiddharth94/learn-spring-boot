d017251bec87b55603c7d3d059fb714eed42d95c
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2023-10-21 21:51:30 +0100

    a command runner to just test db connection

--- learnspring/src/main/java/com/springboot/demo/learnspring/LearnspringApplication.java
+++ learnspring/src/main/java/com/springboot/demo/learnspring/LearnspringApplication.java
@@ -1,7 +1,9 @@
 package com.springboot.demo.learnspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LearnspringApplication {
@@ -10,4 +12,13 @@ public class LearnspringApplication {
 		SpringApplication.run(LearnspringApplication.class, args);
 	}
 
+//	bean for a command line runner app, the code is executed after the Spring Beans have been loaded
+//	& we can use it in this method
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+//		lambda expression syntax; think of it as JS anonymous functions
+		return runner -> {
+			System.out.println("Hello Mars!");
+		};
+	}
 }
