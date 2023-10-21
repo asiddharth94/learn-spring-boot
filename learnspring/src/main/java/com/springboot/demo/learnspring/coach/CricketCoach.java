2c488c784d5b6204bea913bd405fde467d80ef30
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2023-10-21 17:16:24 +0100

    bean scopes example

--- learnspring/src/main/java/com/springboot/demo/learnspring/coach/CricketCoach.java
+++ learnspring/src/main/java/com/springboot/demo/learnspring/coach/CricketCoach.java
@@ -1,8 +1,13 @@
 package com.springboot.demo.learnspring.coach;
 
+//import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+//import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+// for BeanScopeController demo
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CricketCoach implements Coach {
 
     @Override
---
+++ learnspring/src/main/java/com/springboot/demo/learnspring/rest/BeanScopeController.java
@@ -0,0 +1,28 @@
+package com.springboot.demo.learnspring.rest;
+
+import com.springboot.demo.learnspring.coach.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//controller to check Bean Scopes. Needs marking the CricketCoach with @Scope
+@RestController
+public class BeanScopeController {
+
+    private Coach myCoach;
+    private Coach myAnotherCoach;
+
+    //    setting up constructor injection
+    @Autowired
+    public BeanScopeController(@Qualifier("cricketCoach") Coach coach, @Qualifier("cricketCoach") Coach anotherCoach) {
+        myCoach = coach;
+        myAnotherCoach = anotherCoach;
+    }
+
+    @GetMapping("/checkbeanscope")
+    public String check() {
+//        return TRUE for Singleton, FALSE for prototype
+        return "Bean Scope is " + (myCoach == myAnotherCoach);
+    }
+}
